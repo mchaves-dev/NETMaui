879660fa4303a422510286d03a0a2eec3a305647
Maurício Bernardo Chaves <112994981+mchaves-dev@users.noreply.github.com>
2025-09-01 13:33:06 -0300

    add

--- NarutoApp/Pages/Home.xaml.cs
+++ NarutoApp/Pages/Home.xaml.cs
@@ -7,43 +7,66 @@ namespace NarutoApp.Pages;
 
 public partial class Home : ContentPage
 {
-    public Home()
-    {
-        InitializeComponent();
-        LoadCharactersAsync();
-    }
+	private ObservableCollection<Character> _characters = new ObservableCollection<Character>();
+	private int _currentPage = 1;
+	private bool _isBuzy = false;
+
+	public Home()
+	{
+		InitializeComponent();
+		clvCharacters.ItemsSource = _characters; // bind uma vez
+	}
 
 	protected override void OnAppearing()
 	{
 		base.OnAppearing();
-		LoadCharactersAsync();
+		if (_characters.Count == 0)
+			LoadCharacters(_currentPage);
 	}
 
-    private async Task LoadCharactersAsync(int currentPage = 1)
-    {
-        try
-        {
-            using (var httpClient = new HttpClient())
-            {
-                httpClient.BaseAddress = new Uri("https://dattebayo-api.onrender.com");
-                var response = await httpClient.GetAsync($"/characters?page={currentPage}");
+	private async void LoadCharacters(int currentPage = 1)
+	{
+		try
+		{
+			if (IsBusy) return;
 
-                if (response.IsSuccessStatusCode == false)
-                {
-                    await DisplayAlert("Error", $"Ops, Algo não saiu como esperado: {response.StatusCode}", "OK");
-                }
+			_isBuzy = true;
 
-                var characterResponse = await response.Content.ReadFromJsonAsync<CharacterResponse>();
+			using (var httpClient = new HttpClient())
+			{
+				httpClient.BaseAddress = new Uri("https://dattebayo-api.onrender.com");
+				var response = await httpClient.GetAsync($"/characters?page={currentPage}");
 
-                if (characterResponse != null)
-                {
-                    clvCharacters.ItemsSource = new ObservableCollection<Character>(characterResponse.characters);
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            await DisplayAlert("Error", $"Falha ao caregar os personagens: {ex.Message}", "OK");
-        }
-    }
+				if (response.IsSuccessStatusCode == false)
+				{
+					await DisplayAlert("Error", $"Ops, Algo não saiu como esperado: {response.StatusCode}", "OK");
+				}
+
+				var characterResponse = await response.Content.ReadFromJsonAsync<CharacterResponse>();
+
+				if (characterResponse != null)
+				{
+					foreach (var item in characterResponse.characters)
+					{
+						_characters.Add(item);
+					}
+
+					clvCharacters.ItemsSource = _characters;
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Error", $"Falha ao caregar os personagens: {ex.Message}", "OK");
+		}
+		finally
+		{
+			_isBuzy = false;
+		}
+	}
+
+	private void clvCharacters_RemainingItemsThresholdReached(object sender, EventArgs e)
+	{
+		LoadCharacters(_currentPage++);
+	}
 }
