fc2aa9d5e1f95fb079c6042fe93f8d2e39333b40
Maurício Chaves <host.mauricio@gmail.com>
2025-09-01 22:35:46 -0300

    ajustes

--- NarutoApp/Models/Character.cs
+++ NarutoApp/Models/Character.cs
@@ -6,4 +6,10 @@ public sealed class Character
     public string name { get; set; }
     public string []? images { get; set; }
     public string firsImage => images?.FirstOrDefault();
+    public Personal personal { get; set; }
+}
+
+public class Personal
+{
+    public object clan { get; set; }
 }
